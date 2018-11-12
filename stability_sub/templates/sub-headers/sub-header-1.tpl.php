5b87527a453e7dd986733c4aed4677a8d58ec4e1
dalay <dalay@drupalka.ru>
2018-11-12 21:20:34 +0300

    задача по изменению страницы контактов

--- stability_sub/templates/sub-headers/sub-header-1.tpl.php
+++ stability_sub/templates/sub-headers/sub-header-1.tpl.php
@@ -1,12 +1,17 @@
-<?php if((isset($node)) && (gettype($node) == 'object') && ($node->type == 'product')) { $title = $node->model;}; ?>
+<?php
+  if((isset($node)) && (gettype($node) == 'object') && ($node->type == 'product')) { $title = $node->model;};
+  $invite_page = FALSE;
+  if ((isset($node)) && ($node->nid == '96965')) {$invite_page = TRUE;}
+?>
 <section class="page-heading">
   <div class="container">
     <div class="row">
-      <div class="col-md-6">
+      <div class="<?php ($invite_page) ? print 'centered' : print 'col-md-6';?>">
         <?php print render($title_prefix); ?>
         <h1><?php print $title;?></h1>
         <?php print render($title_suffix); ?>
       </div>
+      <?php if ($invite_page == FALSE) :?>
       <div class="col-md-6">
         <span id="gf-role-countdown-role-countdown">
           <?php if (user_has_role(4)) {
@@ -16,6 +21,7 @@
           ?>
         </span>
       </div>
+      <?php endif; ?>
     </div>
   </div>
 </section>
