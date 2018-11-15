8011c50218d087873f67519b9e4034d95f7aa1ef
dalay <dalay@drupalka.ru>
2018-11-15 21:38:31 +0300

    js-скрипт для извратов с меню ассортиментов(которое мужск. и женск.)

---
+++ stability_sub/js/widget_categories.js
@@ -0,0 +1,36 @@
+(function ($) {
+    Drupal.behaviors.gfWidgetCategories = {
+        attach: function (context, settings) {
+
+            // На маленьких экранах переносим блоки ассортиментов
+            // под форму поиска, и делаим их текст "схлопывающимся".
+            if (window.matchMedia("(max-width: 990px)").matches) {
+                $searchForm = $('#views-exposed-form-products-main-catalog');
+
+                if (!$searchForm.length) {
+                    $searchForm = $('#views-exposed-form-products-page-5');
+                }
+
+                $block = $('#block-views-categories-block-5');
+                $collHeader = $('.widget_categories h3', $block);
+
+                $collHeader.next().hide();
+                $collHeader.addClass('collapsible collapsed');
+                $block.appendTo($searchForm);
+
+                $collHeader.click(function () {
+
+                    $header = $(this);
+                    $content = $header.next();
+                    $content.slideToggle(500, function () {
+                        $header.toggleClass('collapsed');
+                    });
+                });
+
+            }
+
+
+        }
+    };
+})(jQuery);
+
