6b465de5cd2379d9c14911033e447eadf1cb0e50
dalay <dalay@drupalka.ru>
2018-12-05 19:47:23 +0300

    added ajax_pager_history.js

---
+++ stability_sub/js/ajax_pager_history.js
@@ -0,0 +1,18 @@
+(function ($) {
+    Drupal.behaviors.gfAjaxPagerHistory = {
+        attach: function (context, settings) {
+
+            // Заносим в историю бр-ра переходы по
+            // ajax-ссылкам пейджера в каталоге.
+            $('ul.pager a', context).each(function() {
+                var path = ($(this).attr('href').substr(0, 1) != '/') ?
+                    '/' + $(this).attr('href') : $(this).attr('href');
+                $(this).click(function() {
+                    window.history.pushState(null, null, path);
+                    return false;
+                });
+            });
+        }
+    };
+})(jQuery);
+
